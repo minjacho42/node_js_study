5f1159968618fb320c623ea19d9e9dcfc30207d3
minjacho42 <minjacho@student.42seoul.kr>
2024-02-26 07:48:30 +0000

    index.js modified to get auth token.

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-import express, { application } from "express"
 
+require("dotenv").config();
+
+let express = require('express');
+const { nextTick } = require("process");
 let app = express();
 app.listen(4242, ()=>console.log("success"));
 app.get("/auth", (req, res)=>
 {
-	res.redirect('https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-d90022c7c1b8bfa4b9c369562bd2c366e5f215c6ba501b1bf8438ce3bc528e32&redirect_uri=http%3A%2F%2F13.124.198.32%3A4242%2Fuser&response_type=code')
+	console.log("redirect to oauth")
+	res.redirect('https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-d90022c7c1b8bfa4b9c369562bd2c366e5f215c6ba501b1bf8438ce3bc528e32&redirect_uri=http%3A%2F%2F13.124.198.32%3A4242%2Fcallback&response_type=code')
 })
+
+const config = {
+	client: {
+		id: process.env.LOGIN_42_API_UID,
+		secret: process.env.LOGIN_42_API_SECRET
+	},
+	auth: {
+		tokenHost: 'https://api.intra.42.fr'
+	}
+}
+
+const { ClientCredentials, ResourceOwnerPassword, AuthorizationCode } = require('simple-oauth2')
 
 app.get("/callback", (req, res)=>{
-	res.send(req.query.code)
+	const client = new AuthorizationCode(config);
+	accessToken = client.getToken({
+		code: req.query.code,
+		redirect_uri: 'http://13.124.198.32:4242/callback'
+	});
+	accessToken.then(
+		result => console.log(result),
+		error => console.log(error)
+	)
+	res.redirect('http://13.124.198.32:4242/user')
 })
 
-app.get("/user", (req, res)=>
-{
-	res.send("User index")
+app.get("/user", (req, res)=>{
+	res.send("user!!")
 })
